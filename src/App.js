12efbc18cff3b0a7520075783725027d899c046c
Jimmy Fikes <jimmyfikes-dev@users.noreply.github.com>
2019-04-05 09:10:15 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import Table from './Table'
+
+const name = 'Jimmy'
+const heading = <h1>Hello, {name}</h1>
+
+class App extends Component {
+    render() {
+      const characters = [
+        {
+          name: 'Charlie', 
+          job: 'Janitor',
+        },
+        {
+          name: 'Mac',
+          job: 'Bouncer',
+        },
+        {
+          name: 'Dee',
+          job: 'Aspring actress',
+        },
+        {
+          name: 'Dennis',
+          job: 'Bartender',
+        },
+      ]
+      return (
+        <div className="App">
+           <h1>{ heading }</h1>
+           <div className="container">
+           <Table characterData={characters} />
+           </div>
+        </div>
+      )
+    }
+  }
+
+  export default App
